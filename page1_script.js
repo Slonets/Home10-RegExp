50ee5a1b49bb467695ac37c8bbbde846e827aab2
Slonets <bogdan.slonets@gmail.com>
2023-12-28 18:44:05 +0200

    Fix next

--- page1_script.js
+++ page1_script.js
@@ -1,21 +1,24 @@
-function validateForm() {
-    // Отримання значень полів форми
-    var email = document.getElementById('email').value;
-    var password = document.getElementById('password').value;
-    var confirmPassword = document.getElementById('confirmPassword').value;
+function validateForm() 
+{
+    // // Отримання значень полів форми
+    // var email = document.getElementById('email').value;
+    // var password = document.getElementById('password').value;
+    // var confirmPassword = document.getElementById('confirmPassword').value;
+
+    let form = document.forms.regForm;
 
     // Перевірка електронної пошти
     var emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
     var emailError = document.getElementById('emailError');
 
-    if (!emailRegex.test(email) || email.length < 4) 
+    if (!emailRegex.test(form.email.value) || form.email.value < 4) 
     {
         emailError.textContent = 'Невірна електронна пошта!';
-        return false;
     }
     else 
-    {
+    {        
         emailError.textContent = ''; // Очистка помилки, якщо введено коректні дані
+        return true;
     }
 
     // Перевірка пароля
@@ -23,27 +26,29 @@ function validateForm() {
 
     var passwordError = document.getElementById('passwordError');
 
-    if (!passwordRegex.test(password)) 
+    if (!passwordRegex.test(form.password.value)) 
     {
         passwordError.textContent = 'Невірний пароль! Пароль повинен містити принаймні 6 символів, 1 літеру нижнього регістра, 1 літеру верхнього регістра та 1 цифру.';
-        return false;
     }
     else 
     {
         passwordError.textContent = '';
+        return true;
     }
 
     // Перевірка підтвердження пароля
-    var confirmPasswordError = document.getElementById('confirmPasswordError');
+    // var confirmPasswordError = document.getElementById('confirmPasswordError');
 
-    if (password !== confirmPassword) 
+    if (form.password.value !== form.confirmPassword.value) 
     {
         confirmPasswordError.textContent = 'Підтвердження пароля не співпадає!';
-        return false;
+        return true;
     }
     else 
     {
+        console.log("Добре! Перевірка пароля");
         confirmPasswordError.textContent = '';
+        return true;
     }
 
     const currentDate = new Date(Date.now());
@@ -51,15 +56,17 @@ function validateForm() {
     expire = currentDate;
     expire.setMinutes(expire.getMinutes() + 10);
 
-    //записуємо в куки
-    document.cookie = `email = ${email}`+ expire.toUTCString()+ "; path='/';";
-    document.cookie = `password = ${epassword}`+ expire.toUTCString()+ ";";
-    
+
     // Якщо всі перевірки пройдені, можна продовжити обробку форми або відправку на сервер
-    if (emailError.textContent === '' && passwordError.textContent === '' && confirmPasswordError.textContent === '') {
+    if (emailError.textContent === '' && passwordError.textContent === '' && confirmPasswordError.textContent === '') 
+    {
         alert('Реєстрація успішна!');
         // Тут ви можете виконати додаткові дії або перейти на іншу сторінку
 
+        //записуємо в куки
+        document.cookie = `email = ${form.email.value}` + expire.toUTCString() + "; path='/';";
+        document.cookie = `password = ${form.password.value}` + expire.toUTCString() + ";";
+        window.location.href = "next_page.html";
         return true;
     }
 }
